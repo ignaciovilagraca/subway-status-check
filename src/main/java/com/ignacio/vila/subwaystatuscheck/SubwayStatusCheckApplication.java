1e6790497626d26997eeff0602f47af799e5b12f
Ignacio Vila <ivila@dc.uba.ar>
2018-04-28 20:45:57 -0300

    Initial commit

---
+++ src/main/java/com/ignacio/vila/subwaystatuscheck/SubwayStatusCheckApplication.java
@@ -0,0 +1,12 @@
+package com.ignacio.vila.subwaystatuscheck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubwayStatusCheckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubwayStatusCheckApplication.class, args);
+	}
+}
