397ee9a054bd617dbe509b0cb22a2e5a9c4b2733
Ignacio Vila <ivila@dc.uba.ar>
2018-05-04 11:39:44 -0300

    var to const

--- src/main/resources/js/Line.js
+++ src/main/resources/js/Line.js
@@ -2,8 +2,8 @@ import React from "react";
 
 export default class Line extends React.Component {
     toMMSS() {
-        var minutes = Math.floor(this.props.line.lineFrequency / 60);
-        var seconds = this.props.line.lineFrequency - minutes * 60;
+        const minutes = Math.floor(this.props.line.lineFrequency / 60);
+        const seconds = this.props.line.lineFrequency - minutes * 60;
         return (minutes > 9 ? minutes : "0" + minutes) + ":" + (seconds > 9 ? seconds : "0" + seconds);
     }
 
